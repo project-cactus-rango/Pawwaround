af755f28efa38e4e71c65b6b8cb53a3fee48cee7
Daniel <djreyes042497@gmail.com>
2020-05-18 11:24:40 -0500

    Add userController functions

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -17,5 +17,27 @@ module.exports = {
         delete newUser[0].password
         req.session.user = newUser[0]
         res.status(201).json(req.session.user)
+    },
+    login: async(req, res) => {
+        const {email, password} = req.body,
+            db = req.app.get('db')
+        
+        let existingUser = await db.users.check_user(email)
+            if(!existingUser[0]){
+                return res.status(400).json('Email not found')
+            }
+
+        const verified = bcrypt.compareSync(password, existingUser[0].password)
+            if(!verified){
+                res.status(401).json('Incorrect Password')
+            }
+
+        delete existingUser[0].password
+        req.session.user = sessionUser
+        res.status(202).json(req.session.user)
+    },
+    logout: (req, res) => {
+        req.session.destroy()
+        res.sendStatus(200)
     }
 }
