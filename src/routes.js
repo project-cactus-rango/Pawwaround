81ee5c287d5a9d7ca34beb242a71bc8257f68e3b
Melissa <Melissalammm@gmail.com>
2020-05-19 10:42:34 -0500

    correct register typo on routes page

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
-import Regiter from "./pages/Register/Register";
+import Register from "./pages/Register/Register";
 import Landing from "./pages/Landing/Landing";
 
 export default function Routes() {
@@ -9,7 +9,7 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={Landing} />
       <Route path="/login" component={Login} />
-      <Route path="/signup" component={Regiter} />
+      <Route path="/signup" component={Register} />
     </Switch>
   );
 }
