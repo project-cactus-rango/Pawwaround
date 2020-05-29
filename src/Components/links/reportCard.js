513b157011a737464d07baf84333a23dea7a2a33
Trevor Ramey <treramey@gmail.com>
2020-05-29 01:32:25 -0500

    fix link card

--- src/Components/links/reportCard.js
+++ src/Components/links/reportCard.js
@@ -5,16 +5,19 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import Link from '@material-ui/core/Link';
-import { Link as RouterLink } from 'react-router-dom';
-import {HashRouter as Router} from 'react-router-dom';
+import Link from "@material-ui/core/Link";
+import { Link as RouterLink } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 245,
   },
   media: {
-    height: 140,
+    height: "100%",
+  },
+  content: {
+    height: "100%",
   },
 });
 
@@ -24,7 +27,7 @@ export default function MediaCard() {
   return (
     <Router>
       <Card className={classes.root} variant="outlined">
-        <CardActionArea>
+        <CardActionArea className={classes.content}>
           <CardMedia
             className={classes.media}
             image="https://www.aaha.org/contentassets/6e767432008443fd978b9cc7ce666e6a/2017-12-27-istock-628950830-abuse-site-blog.jpg"
